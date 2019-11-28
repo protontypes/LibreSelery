8f8226c590da046aabf3c481f6398e1d86b094e3
ly0n <ly0@protonmail.com>
2019-11-28 20:12:58 +0100

    clean commit

---
+++ paypal/paypal.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import paypalrestsdk
+import logging
+
+paypalrestsdk.configure({
+  "mode": "sandbox", # sandbox or live
+
+payment = paypalrestsdk.Payment({
+    "intent": "sale",
+    "payer": {
+        "payment_method": "paypal"},
+    "redirect_urls": {
+        "return_url": "http://localhost:3000/payment/execute",
+        "cancel_url": "http://localhost:3000/"},
+    "transactions": [{
+        "item_list": {
+            "items": [{
+                "name": "item",
+                "sku": "item",
+                "price": "5.00",
+                "currency": "USD",
+                "quantity": 1}]},
+        "amount": {
+            "total": "5.00",
+            "currency": "USD"},
+        "description": "This is the payment transaction description."}]})
+
+if payment.create():
+  print("Payment created successfully")
+else:
+  print(payment.error)
+
