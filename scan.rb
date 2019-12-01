ba88ee204c075f9a48fb6dbbf4f5383f3e6b13d5
lucio.tomys <lucio.tomys@gmail.com>
2019-12-01 22:37:16 +0100

    save depencencies into json file

--- scan.rb
+++ scan.rb
@@ -1,7 +1,16 @@
 #!/usr/bin/env ruby
 
 require 'bibliothecary'
+require 'json'
 
 deps = Bibliothecary.analyse('./')
-puts deps.class
-puts deps[0].class
+
+# convert dict array to json
+json_deps = deps.to_json
+
+#puts json_deps
+
+# Save dependencies into json file
+File.open("dependencies.json","w") do |f|
+    f.write(json_deps)
+end
