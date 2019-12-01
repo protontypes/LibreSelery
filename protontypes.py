c6a73ff2ecba099919ef9d48ee8c1312a6917696
Lucio Tomys <lucio.tomys@gmail.com>
2019-12-01 21:11:04 +0100

    load tokens with json

--- protontypes.py
+++ protontypes.py
@@ -3,13 +3,21 @@
 import requests,os,urllib,posixpath,sys,base64,re
 from urlextract import URLExtract
 from github import Github
+import json
+
+## Load parameters
+with open('tokens.json') as f:
+    tokens_data = json.load(f)
+libraries_api_key = tokens_data["libraries_io_api_key"]
+github_token = tokens_data["github_token"]
+
 
 base_url = 'https://libraries.io/'
 api='api'
 owner='tensorflow'
 name='tensorflow'
 version='latest'
-#libraries_io_api_key = {'api_key':''}
+libraries_io_api_key = {'api_key':libraries_api_key}
 keyword = 'dependencies'
 ok_code = 200
 url_path = posixpath.join(api,'github',owner,name,keyword)
@@ -24,13 +32,23 @@ print(r.json().get('dependencies'))
 repository_url=r.json().get('repository_url')
 print(r.json().keys())
 
-for key in r.json().keys():
-    print()
-    print(key)
-    print(r.json().get(key))
+#for key in r.json().keys():
+#    print()
+#    print(key)
+#    print(r.json().get(key))
+
 
 
 # Not needed
 g = Github()
-repo = g.get_repo(r.json().get("github_id"))
+github_id = int(r.json().get("github_id"))
+print(github_id)
+print(type(github_id))
+
+repo = g.get_repo(github_id)
+
+cotributors = repo.get_contributors()
+
+for contributor in cotributors:
+    print(contributor.email)
 
