9170b2a77a8772d73c33297d89c1e575976daedf
ly0n <ly0@protonmail.com>
2019-12-07 15:44:39 +0100

    now also works for tensorflow

--- protontypes.py
+++ protontypes.py
@@ -10,7 +10,6 @@ parser.add_argument("--project", required=True, type=str, help="Project root fol
 
 args = parser.parse_args()
 root_folder = args.project
-print(root_folder)
 
 ## Load parameters
 with open('tokens.json') as f:
@@ -38,16 +37,16 @@ class GithubConnector:
         return emails_list
 
 
-class LibrariesIOConecter:
+class LibrariesIOConnecter:
     def __init__(self, key):
         self.apiKey = {'api_key':key}
         self.base_url = 'https://libraries.io/'
 
-    def getOwner(self, platform, name):
+    def getOwnerandProject(self, platform, name):
         url_path = posixpath.join('api',platform,name)
         url = urllib.parse.urljoin(self.base_url,url_path)
         r = requests.get(url,params=self.apiKey)
-        if r.status_code is not 200:
+        if r.status_code is not 200 or r.json().get('repository_url') is None:
             print("Request not possible")
             print(r.status_code)
             print(r.text)
@@ -55,12 +54,17 @@ class LibrariesIOConecter:
         else:
             print(r.json().get('repository_url'))
             repository_url=urlparse(r.json().get('repository_url'))
-            owner = repository_url.path.split('/')[-2]
-            return owner
+            owner = repository_url.path.split('/')[1]
+            project_name = repository_url.path.split('/')[2]
+            print("owner")
+            print(owner)
+            print(project_name)
+            return {"owner": owner, "project_name": project_name }
 
     def getDependencyData(self, owner, name):
         url_path = posixpath.join('api','github',owner,name,'dependencies')
         url = urllib.parse.urljoin(self.base_url,url_path)
+        print(url)
         r = requests.get(url,params=self.apiKey)
         if r.status_code is not 200:
             print("Request not possible")
@@ -70,7 +74,7 @@ class LibrariesIOConecter:
         else:
             return {"dependencies": r.json().get('dependencies'), "github_id": r.json().get('github_id')}
 
-librariesIO = LibrariesIOConecter(libraries_api_key)
+librariesIO = LibrariesIOConnecter(libraries_api_key)
 gitConnector = GithubConnector(github_token)
 
 status = subprocess.call('ruby scan.rb --project='+root_folder, shell=True)
@@ -84,8 +88,12 @@ for platform in dependencies_json:
     for deps in platform["dependencies"]:
         name = deps["name"]
         dependency = {"platform": platform_name, "name":name}
-        dependency["owner"] = librariesIO.getOwner(platform_name, name)
-        depData = librariesIO.getDependencyData(dependency["owner"], name)
+        ownerandproject = librariesIO.getOwnerandProject(platform_name, name)
+        if not ownerandproject:
+            break
+        depData = librariesIO.getDependencyData(ownerandproject["owner"],ownerandproject["project_name"])
+        if not depData:
+            break
         dependency["dependencies"] = depData["dependencies"]
         dependency["github_id"] = depData["github_id"]
 
