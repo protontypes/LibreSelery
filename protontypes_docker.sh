7e8c401b5e05efa43df0ca3ef668e72b39ee799b
Tobias Augspurger <ly0@protonmail.com>
2019-12-20 12:00:59 +0000

    moved db to protontypes dotfiles

--- protontypes_docker.sh
+++ protontypes_docker.sh
@@ -4,5 +4,5 @@ echo $cmd
 docker run --rm -it \
 --env-file ~/.protontypes/tokens.env \
 -v $@:$@ \
--v ~/db:/home/proton/.protontypes/db protontypes bash \
+-v ~/.protontypes/db:/home/proton/db protontypes bash \
 -c "$cmd" \
