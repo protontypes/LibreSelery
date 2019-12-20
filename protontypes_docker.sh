838a47f29a67466197449818842ea23ba2a24875
Tobias Augspurger <ly0@protonmail.com>
2019-12-20 08:32:58 +0000

    added history files to git ignore and moved db folder to .protontypes

--- protontypes_docker.sh
+++ protontypes_docker.sh
@@ -4,5 +4,5 @@ echo $cmd
 docker run --rm -it \
 --env-file ~/.protontypes/tokens.env \
 -v $@:$@ \
--v ~/db:/home/proton/db protontypes bash \
+-v ~/db:/home/proton/.protontypes/db protontypes bash \
 -c "$cmd" \
