0a6d8a71d28e30178c9d8f59f14bcbb34d42d449
Tobias Augspurger <ly0@protonmail.com>
2019-12-26 23:44:07 +0000

    Dockerfile CI ready

--- protontypes_docker.sh
+++ protontypes_docker.sh
@@ -2,8 +2,10 @@
 # Start command
 cmd="python proton.py --folder=$@"
 
-docker run --rm \
+docker run --rm -it \
 --env GITHUB_TOKEN=$GITHUB_TOKEN \
 --env LIBRARIES_IO_TOKEN=$LIBRARIES_IO_TOKEN \
+-v $@:$@ \
+-v ~/db:/home/proton/.protontypes/db \
 protontypes \
 bash -c "$cmd" \
