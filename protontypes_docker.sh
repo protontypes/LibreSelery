19889c7678c4bc9b729eb1bf207f39a30191a4e8
T0b14s Augspurger <Ly0@protonmail.com>
2019-12-26 23:24:41 +0100

    Update protontypes_docker.sh

--- protontypes_docker.sh
+++ protontypes_docker.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 cmd="python proton.py --folder=$@"
-docker run --rm -it \
---env GITHUB_TOKEN=$GITHUB_TOKEN \
---env LIBRARIES_IO_TOKEN=$LIBRARIES_IO_TOKEN \
--v $@:$@ \
--v ~/.protontypes/db:/home/proton/db \
-protontypes \
-$cmd \
+docker run --rm \
+  --env GITHUB_TOKEN=$GITHUB_TOKEN \
+  --env LIBRARIES_IO_TOKEN=$LIBRARIES_IO_TOKEN \
+  -v $@:$@ \
+  -v ~/.protontypes/db:/home/proton/db \
+  protontypes \
+  $cmd \
 
