51bdf458a58c339f90537b8abf4d10d2744f6452
T0b14s Augspurger <Ly0@protonmail.com>
2019-12-26 21:40:25 +0100

    Update protontypes_docker.sh

--- protontypes_docker.sh
+++ protontypes_docker.sh
@@ -2,7 +2,7 @@
 cmd="--folder=$@"
 echo $cmd
 docker run --rm \
---env-file ~/.protontypes/tokens.env \
+--env GITHUB_TOKEN=$GITHUB_TOKEN --env LIBRARIES_IO_TOKEN=$LIBRARIES_IO_TOKEN \
 -v $@:$@ \
 -v ~/.protontypes/db:/home/proton/db \
 protontypes \
