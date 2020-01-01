3350fd74ccc57cba8109abf1f2de58ca37a3df32
T0b14s Augspurger <Ly0@protonmail.com>
2020-01-01 21:44:20 +0100

    Update coinbase_pay.py

--- protontypes/payout/coinbase_pay.py
+++ protontypes/payout/coinbase_pay.py
@@ -9,6 +9,6 @@ from coinbase.wallet.client import Client
 
 client = Client(api_key, api_secret)
 account = client.get_primary_account()
-tx = account.send_money(to='@protonmail.com', amount='0.00001', currency='BTC', skip_notifications='True')
+tx = account.send_money(to='@protonmail.com', amount='0.00001', currency='BTC', skip_notifications='true')
 print(tx)
 
