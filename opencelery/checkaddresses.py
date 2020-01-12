ed9b3a248394e399fe19f31a52649a5880c9f393
Tobias Augspurger <ly0@protonmail.com>
2020-01-12 15:17:25 +0000

    removed checkaddress lib file since now in coinbase lib integrated

--- opencelery/checkaddresses.py
+++
@@ -1,4 +0,0 @@
-from coinbase.wallet.client import Client
-client = Client(<api_key>, <api_secret>)
-
-address = client.get_address('2bbf394c-193b-5b2a-9155-3b4732659ede', 'dd3183eb-af1d-5f5d-a90d-cbff946435ff')
