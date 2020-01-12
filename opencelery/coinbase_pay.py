98fd6a8efdbcd3bc08c298858c0e7d39849a161c
Johnny CrckMc <johnny@crckmc.de>
2020-01-12 18:50:59 +0100

    Refactoring to new design

--- opencelery/coinbase_pay.py
+++ opencelery/coinbase_pay.py
@@ -1,21 +1,24 @@
 from coinbase.wallet.client import Client
 import json
 
+
 class CoinbaseConnector:
-    def __init__(self,token,secret):
-        self.client = Client(token,secret)
+    def __init__(self, token, secret):
+        self.client = Client(token, secret)
         self.account = self.client.get_primary_account()
         self.addresses = self.account.get_addresses()
 
-    def isWalletAddress(self,tocheck):
+    def isWalletAddress(self, tocheck):
         for address in tocheck:
             if self.addresses['data'][0]['address'] == tocheck:
                 return True
         return False
 
-    def payout(self,target_email,target_amount='0.00001'):
-        tx = self.account.send_money(to=target_email, amount=target_amount, currency='BTC', skip_notifications='true')
+    def payout(self, target_email, target_amount):
+        tx = self.account.send_money(
+            to=target_email, amount=target_amount, currency='BTC', skip_notifications='false')
         return tx
+
 
 if __name__ == "__main__":
     pass
