a6004a86bd700b194090092ea9e8f4253eb1d521
Tobias Augspurger <ly0@protonmail.com>
2020-01-14 22:17:57 +0000

    better local path integration

--- opencelery_docker.sh
+++ opencelery_docker.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Start command
-cmd="python celery.py --folder=$@"
+cmd="python celery.py --folder=/home/celery/runningrepo/"
 
 docker run --rm \
 --env GITHUB_TOKEN=$GITHUB_TOKEN \
 --env LIBRARIES_IO_TOKEN=$LIBRARIES_IO_TOKEN \
 --env COINBASE_TOKEN=$COINBASE_TOKEN \
 --env COINBASE_SECRET=$COINBASE_SECRET \
+-v $@:/home/celery/runningrepo/ \
 opencelery \
 bash -c "$cmd" \
 
