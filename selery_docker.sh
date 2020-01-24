414c22f51e869d1d58a33de129db43c88e25e620
kikass13 <nick.fiege@streetscooter.eu>
2020-01-24 02:22:08 +0100

    first implementation of proper configuration class and new code structure [OLD CODE CORPSE STIL THERE], still WIP

--- selery_docker.sh
+++ selery_docker.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # Never print SECRET or TOKENS.
 
+TARGET_DIR="/home/selery/runningrepo"
+
 # Mount the argument folder into the container \
 docker run --rm \
 --env GITHUB_TOKEN=$GITHUB_TOKEN \
 --env LIBRARIES_IO_TOKEN=$LIBRARIES_IO_TOKEN \
 --env COINBASE_TOKEN=$COINBASE_TOKEN \
 --env COINBASE_SECRET=$COINBASE_SECRET \
--v $@:/home/selery/runningrepo/ \
+-v $@:$TARGET_DIR \
 openselery \
-bash -c "python selery.py --folder=/home/selery/runningrepo/" \
+bash -c "python selery.py --config $TARGET_DIR/openselery.yml --directory $TARGET_DIR" \
 
