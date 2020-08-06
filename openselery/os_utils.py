7b639784c8894dca10830a35b38e9005c696c285
kikass13 <nick.fiege@streetscooter.eu>
2020-08-06 22:40:44 +0200

    added os_utils module, which contains a pip show command so we can get the version of installed python modules (openselery) faster and better

---
+++ openselery/os_utils.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import subprocess
+
+
+def getPackageInfo(packageName):
+    packageInfo = {}
+    packageInfoProc = subprocess.run(["pip3", "show", packageName], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if(packageInfoProc.stderr == ""):
+        for line in packageInfoProc.stdout.split("\n"):
+            splitStr = line.split(": ")
+            if len(splitStr) >= 2:
+                key = splitStr[0].lower()
+                val = splitStr[1]
+                packageInfo[key] = val
+    return packageInfo
