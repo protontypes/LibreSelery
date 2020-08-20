cc8d91c979ae573f73e6f8e8af8a067f5278f68b
Tobias Augspurger <Ly0@protonmail.com>
2020-08-20 16:55:02 +0200

    Update openselery.py

--- openselery/openselery.py
+++ openselery/openselery.py
@@ -531,7 +531,7 @@ class OpenSelery(object):
 
             self.log("Creating Donation Website")
             donation_website = (
-                "<p align='center'><b>Donate to this address recharge the OpenSelery wallet of OpenSelery:</b><br><b></b><br><b>BTC address:</b><br><b>"
+                "<p align='center'><b>Donate to this address to support OpenSelery:</b><br><b></b><br><b>BTC address:</b><br><b>"
                 + self.config.bitcoin_address
                 + "</b><br><img src='openselery/wallet_qrcode.png'></p>"
             )
