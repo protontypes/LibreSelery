0ead1c9c243f0098d665a0ddb7f5cb2526c6244f
Tobias Augspurger <ly0@protonmail.com>
2020-08-20 12:21:10 +0200

    using tripel quotes to create donation website string

--- openselery/openselery.py
+++ openselery/openselery.py
@@ -507,7 +507,7 @@ class OpenSelery(object):
 
 
             self.log("Creating Donation Website")
-            donation_website = "<p align=\"center\"><b>Donate to this address recharge the OpenSelery wallet of OpenSelery:</b><br><b></b><br><b>BTC address:</b><br><b>\"+self.config.bitcoin_address+\"</b><br><img src=\"../openselery/wallet_qrcode.png\"></p>"
+            donation_website = """<p align="center"><b>Donate to this address recharge the OpenSelery wallet of OpenSelery:</b><br><b></b><br><b>BTC address:</b><br><b>"+self.config.bitcoin_address+"</b><br><img src="../openselery/wallet_qrcode.png"></p>"""
 
             donationPagePath = os.path.join(
                 self.config.result_dir, "donation.md"
