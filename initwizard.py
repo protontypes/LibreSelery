83ccf27bc0442e40927ff80f2ed6f568c549bcae
Arne Döring <arne.doering@gmx.net>
2020-08-25 17:31:16 +0200

    move documentation out of rprompt in the line above.

--- initwizard.py
+++ initwizard.py
@@ -82,95 +82,95 @@ def main():
     print(str(config))
     try:
         answers = {}
+        print("Simulation let you run OpenSelery without payout and coinbase tokens.")
         answer = answerStringToBool(
             prompt(
                 "simulation: ",
                 default=str(config.__dict__["simulation"]),
-                rprompt="Simulation let you run OpenSelery without payout and coinbase tokens.",
                 validator=BoolValidator(),
             )
         )
         inputFeedback(answer)
         answers["simulation"] = answer
 
+        print("Include the target folders contributors.")
         answer = answerStringToBool(
             prompt(
                 "include_main_repository: ",
                 default=str(config.__dict__["include_main_repository"]),
-                rprompt="Include the target folders contributors.",
                 validator=BoolValidator(),
             )
         )
         inputFeedback(answer)
         answers["include_main_repository"] = answer
-
+        print("Include the dependencies of the target folder.")
         answer = answerStringToBool(
             prompt(
                 "include_dependencies: ",
                 default=str(config.__dict__["include_dependencies"]),
-                rprompt="Include the dependencies of the target folder.",
                 validator=BoolValidator(),
             )
         )
         inputFeedback(answer)
         answers["include_dependencies"] = answer
-
+        print(
+            "Include the tooling and runtime from file validator = BoolValidator()) tooling_repos.yml."
+        )
         answer = answerStringToBool(
             prompt(
                 "include_tooling_and_runtime: ",
                 default=str(config.__dict__["include_tooling_and_runtime"]),
-                rprompt="Include the tooling and runtime from file validator = BoolValidator()) tooling_repos.yml.",
             )
         )
         inputFeedback(answer)
         answers["include_tooling_and_runtime"] = answer
 
+        print("Minimum contributions made to be considered.")
         answer = int(
             prompt(
                 "min_contributions_required_payout: ",
                 default=str(config.__dict__["min_contributions_required_payout"]),
-                rprompt="Minimum contributions made to be considered.",
                 validator=IntegerValidator(),
             )
         )
         inputFeedback(answer)
         answers["min_contributions_required_payout"] = answer
 
+        print("Number of randomly selected dependency contributors.")
         answer = int(
             prompt(
                 "included_dependency_contributor: ",
                 default=str(config.__dict__["included_dependency_contributor"]),
-                rprompt="Number of randomly selected dependency contributors.",
                 validator=IntegerValidator(),
             )
         )
         inputFeedback(answer)
         answers["included_dependency_contributor"] = answer
 
+        print("Base weight for all included contributors.")
         answer = int(
             prompt(
                 "uniform_weight: ",
                 default=str(config.__dict__["uniform_weight"]),
-                rprompt="Base weight for all included contributors.",
                 validator=IntegerValidator(),
             )
         )
         inputFeedback(answer)
         answers["uniform_weight"] = answer
 
+        print("Give weight to the last x git commits.")
         answer = prompt(
             "activity_since_commit: ",
             default=str(config.__dict__["activity_since_commit"]),
-            rprompt="Give weight to the last x git commits.",
         )
         inputFeedback(answer)
         answers["activity_since_commit"] = answer
 
+        print("Weight for weighted git commits.")
         answer = int(
             prompt(
                 "activity_weight: ",
                 default=str(config.__dict__["activity_weight"]),
-                rprompt="Weight for weighted git commits.",
                 validator=IntegerValidator(),
             )
         )
@@ -179,12 +179,12 @@ def main():
 
         ## full_split: weighted split over all contributors
         ## random_split: random weighted split with equal payout per contributor
+        print("split mode of salery for contributors.")
         options = ["full", "random"]
         answer = (
             prompt(
                 "split_behavior: ",
                 default=str(config.__dict__["split_behavior"][:-6]),
-                rprompt="split mode of salery for contributors.",
                 validator=WordValidator(options),
                 completer=WordCompleter(options, ignore_case=True),
             ).lower()
@@ -193,87 +193,97 @@ def main():
         inputFeedback(answer)
         answers["split_behavior"] = answer
 
-        answer = float(
-            prompt(
-                "btc_per_picked_contributor: ",
-                default=str(config.__dict__["btc_per_picked_contributor"]),
-                rprompt="The amount of bitcons send per transaction.",
-                validator=DecimalValidator(),
+        if answer == "random_split":
+            print("The amount of bitcons send per transaction.")
+            answer = float(
+                prompt(
+                    "btc_per_picked_contributor: ",
+                    default=str(config.__dict__["btc_per_picked_contributor"]),
+                    validator=DecimalValidator(),
+                )
             )
-        )
-        inputFeedback(answer)
-        answers["btc_per_picked_contributor"] = answer
+            inputFeedback(answer)
+            answers["btc_per_picked_contributor"] = answer
 
+        print("Numbers of total contributors payout per run at random split.")
         answer = int(
             prompt(
                 "number_payout_contributors_per_run: ",
                 default=str(config.__dict__["number_payout_contributors_per_run"]),
-                rprompt="Numbers of total contributors payout per run at random split.",
                 validator=BoolValidator(),
             )
         )
         inputFeedback(answer)
         answers["number_payout_contributors_per_run"] = answer
 
+        print(
+            "The maximum amount of payout per run that is considered for random and full split."
+        )
         answer = float(
             prompt(
                 "payout_per_run: ",
                 default=str(config.__dict__["payout_per_run"]),
-                rprompt="The maximum amount of payout per run that is considered for random and full split.",
                 validator=DecimalValidator(),
             )
         )
         inputFeedback(answer)
         answers["payout_per_run"] = answer
 
+        print(
+            "Bitcoin address to take money from for payout and take money from contributors."
+        )
         answer = prompt(
             "bitcoin_address: ",
             default=str(config.__dict__["bitcoin_address"]),
-            rprompt="Bitcoin address to take money from for payout and take money from contributors.",
             validator=BitcoinAddressValidator(),
         )
         inputFeedback(answer)
         answers["bitcoin_address"] = answer
 
+        print(
+            "Check that the public bitcoin address matches the secret coinbase address."
+        )
         answer = answerStringToBool(
             prompt(
                 "perform_wallet_validation: ",
                 default=str(config.__dict__["perform_wallet_validation"]),
-                rprompt="Check that the public bitcoin address matches the secret coinbase address.",
                 validator=BoolValidator(),
             )
         )
         inputFeedback(answer)
         answers["perform_wallet_validation"] = answer
 
+        print("Skip email sended by coinbase.")
         answer = answerStringToBool(
             prompt(
                 "send_email_notification: ",
                 default=str(config.__dict__["send_email_notification"]),
-                rprompt="Skip email sended by coinbase.",
                 validator=BoolValidator(),
             )
         )
         inputFeedback(answer)
         answers["send_email_notification"] = answer
 
+        print("Message send with every coinbase email. Never send an URL.")
         answer = prompt(
             "optional_email_message: ",
             default=str(config.__dict__["optional_email_message"]),
-            rprompt="Message send with every coinbase email. Never send an URL.",
         )
         inputFeedback(answer)
         answers["optional_email_message"] = answer
 
-        print("pretending to write full config:")
-        print(str(answers))
-
+        hasChanges = false
         for key in answers:
             if answers[key] != config.__dict__[key]:
+                if not hasChanges:
+                    print("The following changes have been made:")
+                    hasChanges = true
                 print(
                     "change %s from %s to %s"
                     % (key, config.__dict__[key], answers[key])
                 )
+        if not hasChanges:
+            print("using default configuration")
 
     except KeyboardInterrupt:
         print("Setup canceled, nothing is safed.")
